df82169761b21c7ca6c348ed7d4b6d7dacde4a63
MateusGcB <127869997+MateusGcB@users.noreply.github.com>
2023-06-13 12:22:33 -0300

    Update index.js

--- fatorial/index.js
+++ fatorial/index.js
@@ -1,9 +1,17 @@
 function Fatorial () {
         
+          // Obtem o elemento
+        
       const inputNumber = document.getElementById( "Inserir" );
       const result = document.getElementById( "Resultado" );
+        
+        // Obtem o valor e converte para um numero inteiro
+        
       const number = parseInt( inputNumber.value );
 
+        
+        // Verifica se o número e valido
+        
       if ( isNaN ( number ) || number < 0 ) {
 
         result.innerHTML = "Esse numero é invalido";
@@ -11,14 +19,20 @@ function Fatorial () {
 
       }
 
+         // Começa o fatorial com 1
+        
       let factorial = 1;
 
+         // Calcula o fatorial
+        
       for ( let i = number; i >= 1; i-- ) {
 
         factorial *= i;
 
       }
 
+        // Mostra o resultado 
+        
       result.innerHTML = `O resuldado do fatorial de ${number} é ${factorial}.`;
 
     }
