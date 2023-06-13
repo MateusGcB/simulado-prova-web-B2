cc4226db93cca3c988f05204866911f67db3885f
MateusGcB <127869997+MateusGcB@users.noreply.github.com>
2023-06-13 12:17:00 -0300

    Update index.js

--- Primo/index.js
+++ Primo/index.js
@@ -1,11 +1,17 @@
-function isPrime ( number ) {
+function PRIMO ( number ) {
 
+      
+        // Confere se o numero e menor que 2
+      
       if ( number < 2 ) {
 
         return false;
 
       }
 
+      
+      // Ve se o numero e divisivel por um número de 2 ate a raiz quadrada
+      
       for ( let i = 2; i <= Math.sqrt(number); i++ ) {
 
         if ( number % i === 0 ) {
@@ -20,6 +26,7 @@ function isPrime ( number ) {
     }
 
 
+      // Checa se o numero e primo
 
     function Checagem () {
 
@@ -27,11 +34,15 @@ function isPrime ( number ) {
       const result = document.getElementById( "Resultado" );
       const number = parseInt( inputNumber.value );
 
-      if ( isPrime ( number )) {
+          // Da a resposta afirmativa se o numero for primo
+          
+      if ( PRIMO ( number )) {
 
         result.innerHTML = number + " é primo.";
       } 
       
+           // Da a resposta negativa se o numero nao e primo
+          
       else {
 
         result.innerHTML = number + " não é primo.";
